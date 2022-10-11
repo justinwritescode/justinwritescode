65a9cf9ddcb4683cf671af48baba9bf0da151964
Justin <justin@thebackroom.app>
2022-10-11 10:51:03 -0400

    Forgot the most important plugin!

--- install-mkdocs-plugins.sh
+++ install-mkdocs-plugins.sh
@@ -1,4 +1,5 @@
 echo "Installing mkdocs plugins..."
+pip install mkdocs material
 pip install mkdocs-rss-plugin
 pip install mkdocs-git-revision-date-localized-plugin
 pip install mkdocs-categories-plugin
@@ -6,7 +7,5 @@ pip install mkdocs-blog-plugin
 pip install mkdocs-schema-reader
 pip3 install mkpdfs-mkdocs
 pip install mkdocs-with-pdf
-pip install pango
-pip3 install weasyprint
 pip install mkdocs-multirepo-plugin
 pip install mkdocs-section-index
