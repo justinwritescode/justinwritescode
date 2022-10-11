68f106923a8d7b8005c5e9fabe0ee995c9d86f1b
Justin <justin@thebackroom.app>
2022-10-11 10:48:31 -0400

    synchronizing with submodules and making a tiny change to the Pages CD

---
+++ install-mkdocs-plugins.sh
@@ -0,0 +1,12 @@
+echo "Installing mkdocs plugins..."
+pip install mkdocs-rss-plugin
+pip install mkdocs-git-revision-date-localized-plugin
+pip install mkdocs-categories-plugin
+pip install mkdocs-blog-plugin
+pip install mkdocs-schema-reader
+pip3 install mkpdfs-mkdocs
+pip install mkdocs-with-pdf
+pip install pango
+pip3 install weasyprint
+pip install mkdocs-multirepo-plugin
+pip install mkdocs-section-index
