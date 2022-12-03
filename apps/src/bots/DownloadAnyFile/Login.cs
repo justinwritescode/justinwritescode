39a09d1f43c65227cba78c727ff8398d4ab1f0e8
David <justin@justinwritescode.com>
2022-12-03 02:26:32 -0500

    some scripts changes

---
+++ apps/src/bots/DownloadAnyFile/Login.cs
@@ -0,0 +1,16 @@
+/* 
+ * Login.cs
+ * 
+ *   Created: 2022-11-28-06:00:44
+ *   Modified: 2022-11-28-06:00:44
+ * 
+ *   Author: Justin Chase <justin@justinwritescode.com>
+ *   
+ *   Copyright © 2022 Justin Chase, All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */ 
+
+public class Login
+{
+	
+}
